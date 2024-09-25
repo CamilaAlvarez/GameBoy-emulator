fe8cf603b8a27fd82cc05b090e72cea22e3b0166
Camila Alvarez <cam.alvarez.i@gmail.com>
2024-09-24 22:34:39 -0300

    Working with the stack

--- src/cpu.rs
+++ src/cpu.rs
@@ -8,6 +8,8 @@ pub struct CPU {
     registers: Registers,
     // current instruction in execution
     pc: u16,
+    // stack pointer, pointer grows downwards (away from the end of memory)
+    sp: u16,
     bus: MemoryBus,
 }
 impl CPU {
@@ -19,11 +21,77 @@ impl CPU {
                         let value = self.registers.c;
                         let new_value = self.add(value);
                         self.registers.a = new_value;
+                        // move to the next instruction and return the new pc
+                        // wrapping add wraps the result => 255 + 1 = 0
+                        self.pc.wrapping_add(1)
                     }
-                    _ => { }
+                    _ => { self.pc }
                 }
             }
-            _ => { }
+            Instruction::PUSH(target) => {
+                let value = match target {
+                    StackTarget::BC => self.registers.get_bc(),
+                    _ => { panic!("TODO: support more targets") }
+                };
+                self.push(value);
+                self.pc.wrapping_add(1)
+            }
+            Instruction::POP(target) => {
+                let result = self.pop();
+                match target {
+                    StackTarget::BC => self.registers.set_bc(result),
+                    _ => { panic!("TODO: support more targets") }
+                };
+                self.pc.wrapping_add(1);
+            }
+            Instruction::JP(test) => {
+                let jump_condition = match test {
+                    JumpTest::NotZero => !self.registers.f.zero,
+                    JumpTest::NotCarry => !self.registers.f.carry,
+                    JumpTest::Zero => self.registers.f.zero,
+                    JumpTest::Carry => self.registers.f.carry,
+                    JumpTest::Always => true
+                };
+                self.jump(jump_condition)
+            }
+            Instruction::LD(load_type) => {
+                match load_type {
+                    LoadType::Byte(target, source) => {
+                       let source_value = match source {
+                           LoadByteSource::A => self.registers.a,
+                           LoadByteSource::D8 => self.read_next_byte(),
+                           // HL is the source register
+                           LoadByteSource::HLI => self.bus.read_byte(self.registers.get_hl()),
+                           _ => { panic!("TODO: Implement other sources") }
+                       };
+                       match target {
+                            LoadByteTarget::A => self.registers.a = source_value,
+                            LoadByteTarget::HLI => self.bus.write_byte(self.registers.get_hl(), source_value),
+                            _ => { panic!("TODO: Implement other targets") }
+                       };
+                       match source {
+                           LoadByteSource::D8 => self.pc.wrapping_add(2),
+                           _                  => self.pc.wrapping_add(1),
+                       }
+                    }
+                    _ => { panic!("TODO: implement other load types") }
+                }
+            }
+            _ => { self.pc }
+        }
+    }
+
+    fn jump(&mut self, should_jump: bool) -> u16 {
+        if should_jump {
+            // Gameboy is little endian so read pc + 2 as most significant bit
+            // and pc + 1 as least significant bit
+            let least_significant_byte = self.bus.read_byte(self.pc + 1) as u16;
+            let most_significant_byte = self.bus.read_byte(self.pc + 2) as u16;
+            (most_significant_byte << 8) | least_significant_byte
+        } else {
+            // even if we don't jump we still need to move the pc (the instruction
+            // is 3 bytes wide)
+            self.pc.wrapping_add(3)
         }
     }
 
@@ -38,15 +106,41 @@ impl CPU {
         new_value
     }
 
-    fn setp(&mut self) {
+    fn step(&mut self) {
         let mut instruction_byte = self.bus.read_byte(self.pc);
+        let prefixed = instruction_byte == 0xCB;
+        if prefixed {
+            // TODO: maybe we should do a wrapping_add? NO! Because it needs to be the next
+            // instruction
+            instruction_byte = self.bus.read_byte(self.pc + 1);
+        }
 
-        let next_pc 0 if let Some(instruction) = Instruction::from_byte(instruction_byte) {
+        let next_pc 0 if let Some(instruction) = Instruction::from_byte(instruction_byte, prefixed) {
             self.execite(instruction);
         } else {
-            panic!("Unknown instruction found for: 0x{:x}", instruction_byte);
+            let description = format!("0x{}{:x}", if prefixed { "cb" } else { "" }, instruction_byte);
+            panic!("Unknown instruction found for: {}", description);
         };
         
         self.pc = next_pc;
+    }
+
+    fn push(&mut self, value: u16) {
+        self.sp = self.sp.wrapping_sub(1);
+        // Most significant byte 
+        self.bus.write_byte(self.sp, ((value & 0xFF00) >> 8) as u8);
+
+        self.sp = self.sp.wrapping_sub(1);
+        self.bus.write_byte(self.sp, ((value && 0x00FF) as u8);
+    }
+
+    fn pop(&mut self) -> u16 {
+        let lsb = self.bus.read_byte(self.sp) as u16;
+        self.sp = self.sp.wrapping_add(1);
+
+        let msb = self.bus.read_byte(self.sp) as u16;
+        self.sp = self.sp.wrapping_add(1);
+
+        (msb << 8) | lsb
     }
 }
--- src/cpu/instructions.rs
+++ src/cpu/instructions.rs
@@ -2,6 +2,11 @@ enum Instruction {
     // Add ArithmeticTarget to the value stored in A, then
     // store the result in A
     ADD(ArithmeticTarget),
+    INC(IncDecTarget),
+    RLC(PrefixTarget),
+    JP(JumpTest),
+    LD(LoadType),
+    PUSH(StackTarget),
 }
 
 enum ArithmeticTarget {
@@ -12,13 +17,54 @@ enum IncDecTarget {
     BC, DE
 }
 
-impl Instruction {
-    pub fn from_byte(byte: u8) -> Option<Instruction> {
-        match byte {
-            0x02 => Some(Instruction::INC(IncDecTarget::BC)),
-            0x13 => Some(Instruction::INC(IncDecTarget::DE)),
-            _ => None
+enum PrefixTarget {
+    B
+}
 
+enum JumpTest {
+    NotZero,
+    Zero,
+    NotCarry,
+    Carry,
+    Always,
+}
+
+enum LoadByteTarget {
+    A, B, C, D, E, H, L, HLI
+}
+
+enum LoadByteSource {
+    A, B, C, D, E, H, L, D8, HLI
+}
+
+enum LoadType {
+    Byte(LoadByteTarget, LoadByteSource),
+}
+
+enum StackTarget {
+    BC
+}
+
+impl Instruction {
+    pub fn from_byte(byte: u8, prefixed: bool) -> Option<Instruction> {
+        if prefixed {
+            Instruction::from_byte_prefixed(byte)
+        } else {
+            Instruction::from_byte_not_prefixed(byte)
+        }
+    }
+
+    fn from_byte_prefixed(byte: u8) -> Option<Instruction> {
+        match byte {
+            0x00 => Some(Instruction::RLC(PrefixTarget::B)),
+            _ => None
+        }
+    }
+
+    fn from_byte_not_prefixed(byte: u8) -> Option<Instruction> {
+        match byte {
+            0x00 => Some(Instruction::INC(IncDecTarget::BC)),
+            _ => None
         }
     }
 }
